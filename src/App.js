69632a6c6d806ef150f1c72b707e534e107fe176
Aditya <adikpote@gmail.com>
2022-12-09 00:16:01 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+          <span style={{color: 'white'}}>Aditya Pote</span>
+    </div>
+  );
+}
+
+export default App;
